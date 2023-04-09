e53a78b4162e79b204bdf44f2426f152a6f99ac7
ShadyResurrected <cchandra554@gmail.com>
2023-04-09 19:20:58 +0530

    Minor changes
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -1,15 +1,18 @@
+import { Tooltip } from '@mui/material'
 import React from 'react'
 
 const NavigationDots = ({ active }) => {
     return (
         <div className='app__navigation'>
             {['home', 'about', 'work', 'skills', 'contact'].map((item,index) => (
+                <Tooltip title={item.toUpperCase()} placement="left-start">
                     <a 
                     href={`#${item}`} 
                     key={item+index} 
                     className='app__navigation-dot'
-                    style={active === item ? {backgroundColor: '#313BAC'}:{}} 
+                    style={active === item ? {backgroundColor: '#ffc9c9'}:{}} 
                     onClick={() => setToggle(false)}/>
+                    </Tooltip>
             ))}
         </div>
     )
